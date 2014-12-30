489a5f74713181911e0291657cb1147419688baa
Chris Boese <chris107565@gmail.com>
2014-12-30 15:31:40 -0800

    heroku prepped & ready for deploy

--- rest-api/src/main/java/org/oregonask/Application.java
+++ rest-api/src/main/java/org/oregonask/Application.java
@@ -1,6 +1,5 @@
 package org.oregonask;
 
-import static spark.SparkBase.setIpAddress;
 import static spark.SparkBase.setPort;
 import static spark.SparkBase.staticFileLocation;
 
@@ -17,8 +16,16 @@ public class Application {
 
 	public static void main(String[] args) {
 	    
-		setIpAddress("localhost");
-		setPort(8080);
+		//Heroku assigns different port each time, hence reading it from process.
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
+        }
+        setPort(port);
+        
 		staticFileLocation("/public/app");
 		
 		
